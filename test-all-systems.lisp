ac6e71431255b35c2ac7b641af183a9b38b23c9c
Pascal J. Bourguignon <pjb@informatimago.com>
2013-06-20 22:26:07 +0200

    Added header and license.

--- test-all-systems.lisp
+++ test-all-systems.lisp
@@ -1,3 +1,37 @@
+;;;; -*- mode:lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               test-all-systems.lisp
+;;;;LANGUAGE:           Common-Lisp
+;;;;SYSTEM:             Common-Lisp
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    A script loading all my systems verbosely, to check they all compile.
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal. Bourguignon <pascal.bourguignon@ubudu.com>
+;;;;MODIFICATIONS
+;;;;    2013-06-20 <PJB> Recently created.
+;;;;BUGS
+;;;;LEGAL
+;;;;    AGPL3
+;;;;    
+;;;;    Copyright Pascal. Bourguignon 2013 - 2013
+;;;;    
+;;;;    This program is free software: you can redistribute it and/or modify
+;;;;    it under the terms of the GNU Affero General Public License as published by
+;;;;    the Free Software Foundation, either version 3 of the License, or
+;;;;    (at your option) any later version.
+;;;;    
+;;;;    This program is distributed in the hope that it will be useful,
+;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;;    GNU Affero General Public License for more details.
+;;;;    
+;;;;    You should have received a copy of the GNU Affero General Public License
+;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;**************************************************************************
+;;;;    
 (ql:register-local-projects)
 
 (defparameter *systems*
