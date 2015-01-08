2f7541aa5c6397bea654d392afef9b3b6aecd5fc
Pascal J. Bourguignon <pjb@informatimago.com>
2015-01-08 23:07:06 +0100

    Extracted test out to a separate test system.

---
+++ common-lisp/cesarum/index-set-test.lisp
@@ -0,0 +1,132 @@
+;;;; -*- mode:lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               index-set-test.lisp
+;;;;LANGUAGE:           Common-Lisp
+;;;;SYSTEM:             Common-Lisp
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    The tests for index-set.lisp
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
+;;;;MODIFICATIONS
+;;;;    2015-01-08 <PJB> Extracted from index-set.lisp
+;;;;BUGS
+;;;;LEGAL
+;;;;    AGPL3
+;;;;    
+;;;;    Copyright Pascal J. Bourguignon 2015 - 2015
+;;;;    
+;;;;    This program is free software: you can redistribute it and/or modify
+;;;;    it under the terms of the GNU Affero General Public License as published by
+;;;;    the Free Software Foundation, either version 3 of the License, or
+;;;;    (at your option) any later version.
+;;;;    
+;;;;    This program is distributed in the hope that it will be useful,
+;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;;    GNU Affero General Public License for more details.
+;;;;    
+;;;;    You should have received a copy of the GNU Affero General Public License
+;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;**************************************************************************
+
+(in-package "COM.INFORMATIMAGO.COMMON-LISP.CESARUM.INDEX-SET")
+
+
+;;----------------------------------------------------------------------
+;; RANGE TESTS
+
+(define-test test/range ()
+  (assert-true (range-emptyp (make-range :start 1 :count 0)))
+  (assert-true (range-emptyp (make-range :start 1 :last 0)))
+  (assert-true (range-emptyp (make-range :start 1 :end 1)))
+  (assert-true (not (range-emptyp (make-range :start 1 :count 1))))
+  (assert-true (not (range-emptyp (make-range :start 1 :last 1))))
+  (assert-true (not (range-emptyp (make-range :start 1 :end 2))))
+  (assert-true (equal-range (make-range :start 10 :end 21)
+                            (make-range :first 10 :last 20)))
+  (assert-true (equal-range (make-range :start 0 :end -1)
+                            (make-range :first 0 :last -1)))
+  
+  (test = (range-start (make-range :start 1 :count 3)) 1)
+  (test = (range-last  (make-range :start 1 :count 3)) 3)
+  (test = (range-end   (make-range :start 1 :count 3)) 4)
+  (test = (range-count (make-range :start 1 :count 3)) 3)
+  (test = (range-start (copy-range (make-range :start 1 :count 3))) 1)
+  (test = (range-last  (copy-range (make-range :start 1 :count 3))) 3)
+  (test = (range-end   (copy-range (make-range :start 1 :count 3))) 4)
+  (test = (range-count (copy-range (make-range :start 1 :count 3))) 3)
+
+  (test = (range-start (make-range :start 11 :last 13)) 11)
+  (test = (range-last  (make-range :start 11 :last 13)) 13)
+  (test = (range-end   (make-range :start 11 :last 13)) 14)
+  (test = (range-count (make-range :start 11 :last 13))  3)
+  (test = (range-start (copy-range (make-range :start 11 :last 13))) 11)
+  (test = (range-last  (copy-range (make-range :start 11 :last 13))) 13)
+  (test = (range-end   (copy-range (make-range :start 11 :last 13))) 14)
+  (test = (range-count (copy-range (make-range :start 11 :last 13)))  3)
+
+  (test = (range-start (make-range :start 11 :end 14)) 11)
+  (test = (range-last  (make-range :start 11 :end 14)) 13)
+  (test = (range-end   (make-range :start 11 :end 14)) 14)
+  (test = (range-count (make-range :start 11 :end 14))  3)
+  (test = (range-start (copy-range (make-range :start 11 :end 14))) 11)
+  (test = (range-last  (copy-range (make-range :start 11 :end 14))) 13)
+  (test = (range-end   (copy-range (make-range :start 11 :end 14))) 14)
+  (test = (range-count (copy-range (make-range :start 11 :end 14)))  3)
+
+  (test = (range-start (make-range :count 3 :last 13)) 11)
+  (test = (range-last  (make-range :count 3 :last 13)) 13)
+  (test = (range-end   (make-range :count 3 :last 13)) 14)
+  (test = (range-count (make-range :count 3 :last 13))  3)
+  (test = (range-start (copy-range (make-range :count 3 :last 13))) 11)
+  (test = (range-last  (copy-range (make-range :count 3 :last 13))) 13)
+  (test = (range-end   (copy-range (make-range :count 3 :last 13))) 14)
+  (test = (range-count (copy-range (make-range :count 3 :last 13)))  3)
+
+  (test = (range-start (make-range :count 3 :end 14)) 11)
+  (test = (range-last  (make-range :count 3 :end 14)) 13)
+  (test = (range-end   (make-range :count 3 :end 14)) 14)
+  (test = (range-count (make-range :count 3 :end 14))  3)
+  (test = (range-start (copy-range (make-range :count 3 :end 14))) 11)
+  (test = (range-last  (copy-range (make-range :count 3 :end 14))) 13)
+  (test = (range-end   (copy-range (make-range :count 3 :end 14))) 14)
+  (test = (range-count (copy-range (make-range :count 3 :end 14)))  3))
+
+
+(define-test test/range/complement ()
+  
+  (test equal-ranges
+        (complement-ranges (vector) 0 100)
+        (vector (make-range :start 0 :end 100)))
+
+  (test equal-ranges
+        (complement-ranges (vector  (make-range :start 0 :end 100)) 0 100)
+        (vector))
+
+  (test equal-ranges
+        (complement-ranges (vector (make-range :start 0 :end 90)) 0 100)
+        (vector (make-range :start 90 :end 100)))
+  
+  (test equal-ranges
+        (complement-ranges (vector (make-range :start 10 :end 100)) 0 100)
+        (vector (make-range :start 0 :end 10)))
+
+  (test equal-ranges
+        (complement-ranges (vector (make-range :start 10 :end 90)) 0 100)
+        (vector (make-range :start 0 :end 10)  (make-range :start 90 :end 100)))
+
+  (expect-condition error
+                    (complement-ranges (vector  (make-range :start 0 :end 100)) 10 90)))
+
+;;----------------------------------------------------------------------
+;; INDEX-SET TESTS
+
+(defun test/all ()
+  (test/range)
+  (test/range/complement)
+  (com.informatimago.common-lisp.cesarum.set::test/all/class 'index-set))
+
+
---
+++ common-lisp/cesarum/set-test.lisp
@@ -0,0 +1,271 @@
+;;;; -*- mode:lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               set-test.lisp
+;;;;LANGUAGE:           Common-Lisp
+;;;;SYSTEM:             Common-Lisp
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    The tests of set.lisp
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
+;;;;MODIFICATIONS
+;;;;    2015-01-08 <PJB> Extracted from set.lisp
+;;;;BUGS
+;;;;LEGAL
+;;;;    AGPL3
+;;;;    
+;;;;    Copyright Pascal J. Bourguignon 2015 - 2015
+;;;;    
+;;;;    This program is free software: you can redistribute it and/or modify
+;;;;    it under the terms of the GNU Affero General Public License as published by
+;;;;    the Free Software Foundation, either version 3 of the License, or
+;;;;    (at your option) any later version.
+;;;;    
+;;;;    This program is distributed in the hope that it will be useful,
+;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;;    GNU Affero General Public License for more details.
+;;;;    
+;;;;    You should have received a copy of the GNU Affero General Public License
+;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;**************************************************************************
+
+(in-package "COM.INFORMATIMAGO.COMMON-LISP.CESARUM.SET")
+
+;;;-----------------------------------------------------------------------
+;;; TESTS
+;;;-----------------------------------------------------------------------
+
+(defun test-sets (test-class)
+  (list '() '(1) '(1 2 3)
+        '#() '#(1) '#(1 2 3)
+        (copy test-class '()) (copy test-class '(1)) (copy test-class '(1 2 3))))
+
+(define-test test/all/nil ()
+  (loop
+    :for seq :in (test-sets 'list-set)
+    :do
+    (test eql (map-elements nil (function identity) seq) nil)
+    (test set-equal (let ((result '()))
+                      (map-elements nil (lambda (element) (push element result)) seq)
+                      result)
+          seq)))
+
+(define-test test/map-elements (test-class)
+  (loop
+    :for set :in (test-sets test-class)
+    :do (loop
+          :for class :in (list 'list 'vector test-class)
+          :do (test set-equal (map-elements class (function identity) set)
+                    (ecase (cardinal set)
+                      (0 '())
+                      (1 '(1))
+                      (3 '(1 2 3)))))))
+
+(define-test test/copy (test-class)
+  (loop
+    :for (expected type original)
+    :in (list (list nil        'nil       '(1 2 3 4))
+              (list '(1 2 3 4) 'list      '(1 2 3 4))
+              (list '(1 2 3 4) 'vector    '(1 2 3 4))
+              (list '(1 2 3 4) test-class '(1 2 3 4)))
+    :do
+    (test set-equal               (copy type original)  expected (type original))
+    (test set-equal (copy 'list   (copy type original)) expected (type original))
+    (test set-equal (copy 'vector (copy type original)) expected (type original))))
+
+
+(define-test test/is-subseq (test-class1 test-class2)
+  (flet ((test-set1 (&rest elements)
+           (copy test-class1 elements))
+         (test-set2 (&rest elements)
+           (copy test-class2 elements)))
+    (assert-true (is-subset (test-set1)
+                            (test-set2)))
+    (assert-true (is-subset (test-set1 1)
+                            (test-set2 1)))
+    (assert-true (is-subset (test-set1 1 2 3)
+                            (test-set2 1 2 3)))
+    (assert-true (is-subset (test-set1 1 2 3  11 12 13)
+                            (test-set2 11 12 13 1 2 3)))
+    (assert-true (is-subset (test-set1)
+                            (test-set2 1)))
+    (assert-true (not (is-subset (test-set1 1)
+                                 (test-set2))))
+    (assert-true (not (is-subset (test-set1 1)
+                                 (test-set2 2))))
+    (assert-true (is-subset (test-set1 1 2 3)
+                            (test-set2 1 2 3 4)))
+    (assert-true (not (is-subset (test-set1 1 2 3 4)
+                                 (test-set2 1 2 3))))))
+
+
+(define-test test/set-equal (test-class)
+  (flet ((test-set (&rest elements)
+           (copy test-class elements)))
+    (assert-true (set-equal (test-set)
+                            (test-set)))
+    (assert-true (set-equal (test-set 1)
+                            (test-set 1)))
+    (assert-true (set-equal (test-set 1 2 3)
+                            (test-set 1 2 3)))
+    (assert-true (set-equal (test-set 1 2 3  11 12 13)
+                            (test-set 11 12 13 1 2 3)))
+    (assert-true (not (set-equal (test-set)
+                                 (test-set 1))))
+    (assert-true (not (set-equal (test-set 1)
+                                 (test-set))))
+    (assert-true (not (set-equal (test-set 1)
+                                 (test-set 2))))
+    (assert-true (not (set-equal (test-set 1 2 3)
+                                 (test-set 1 2 3 4))))
+    (assert-true (not (set-equal (test-set 1 2 3 4)
+                                 (test-set 1 2 3))))))
+
+
+
+(define-test test/union (operator test-class)
+  (flet ((test-set (&rest elements)
+           (copy test-class elements))) 
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3 7 8 10 11 12)
+                             (test-set 1 2 3 7 8 10 11 12))
+          (test-set 1 2 3 7 8 10 11 12))
+
+    (test set-equal (funcall operator 
+                             (test-set)
+                             (test-set 1 2 3 7 8 10 11 12))
+          (test-set 1 2 3 7 8 10 11 12))
+
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3 7 8 10 11 12)
+                             (test-set))
+          (test-set 1 2 3 7 8 10 11 12))
+
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3 7 8 10 11 12)
+                             (test-set 0 4 5 6 9 10))
+          (test-set 0 1 2 3 4 5 6 7 8 9 10 11 12))
+
+    (test set-equal (funcall operator 
+                             (test-set 10 11 12)
+                             (test-set 1 2 3 7 8))
+          (test-set 1 2 3 7 8 10 11 12))
+
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3 7 8)
+                             (test-set 10 11 12))
+          (test-set 1 2 3 7 8 10 11 12))
+
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3 5 6 7)
+                             (test-set 3 4 5 7 8 9  12 13))
+          (test-set 1 2 3 4 5 6 7 8 9 12 13))
+
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3 5 6 7  12 13)
+                             (test-set 3 4 5 7 8 9))
+          (test-set 1 2 3 4 5 6 7 8 9 12 13))
+
+    (test set-equal (funcall operator 
+                             (test-set 1 2 3  11 12 13)
+                             (test-set 3 4 5  13 14 15))
+          (test-set 1 2 3 4 5 11 12 13 14 15))
+    
+    (test set-equal (funcall operator 
+                             (test-set 3 4 5  13 14 15)
+                             (test-set 1 2 3  11 12 13))
+          (test-set 1 2 3 4 5 11 12 13 14 15))))
+
+
+
+
+
+(define-test test/intersection (operator test-class)
+  (flet ((test-set (&rest elements)
+           (copy test-class elements)))
+    (test set-equal (funcall operator
+                             (test-set 1 2 3 7 8 10 11 12)
+                             (test-set 1 2 3 7 8 10 11 12))
+          (test-set 1 2 3 7 8 10 11 12))
+
+    (test set-equal (funcall operator
+                             (test-set)
+                             (test-set 1 2 3 7 8 10 11 12))
+          (test-set))
+
+    (test set-equal (funcall operator
+                             (test-set 1 2 3 7 8 10 11 12)
+                             (test-set))
+          (test-set))
+
+    (test set-equal (funcall operator
+                             (test-set 1 2 3 7 8 10 11 12)
+                             (test-set 0 4 5 6 9 10))
+          (test-set 10))
+
+    (test set-equal (funcall operator
+                             (test-set 10 11 12)
+                             (test-set 1 2 3 7 8))
+          (test-set))
+
+    (test set-equal (funcall operator
+                             (test-set 1 2 3 7 8)
+                             (test-set 10 11 12))
+          (test-set))
+
+    (test set-equal (funcall operator
+                             (test-set 1 2 3 5 6 7)
+                             (test-set 3 4 5 7 8 9  12 13))
+          (test-set 3 5 7))
+
+    (test set-equal (funcall operator
+                             (test-set 1 2 3 5 6 7  12 13)
+                             (test-set 3 4 5 7 8 9))
+          (test-set 3 5 7))
+
+    (test set-equal (funcall operator
+                             (test-set 1 2 3  11 12 13)
+                             (test-set 3 4 5  13 14 15))
+          (test-set 3 13))
+    
+    (test set-equal (funcall operator
+                             (test-set 3 4 5  13 14 15)
+                             (test-set 1 2 3  11 12 13))
+          (test-set 3 13))))
+
+
+
+(define-test test/all/sequence (test-class)
+  "All the tests working on LIST or VECTOR as sets."
+  (test/is-subseq test-class test-class)
+  (test/set-equal test-class)
+  (test/copy        test-class)
+  (test/map-elements test-class))
+
+(define-test test/all/class (test-class)
+  "All the tests working on set classes."
+  (test/all/sequence test-class)
+  (test/is-subseq test-class 'list)
+  (test/is-subseq test-class 'vector)
+  (test/is-subseq 'list   test-class)
+  (test/is-subseq 'vector test-class)
+  (test/union (function merge) test-class)
+  (test/union (curry (function union) test-class) test-class)
+  (test/union (curry (function union) 'vector) test-class)
+  (test/intersection (function intersect) test-class)
+  (test/intersection (curry (function intersection) test-class) test-class)
+  (test/intersection (curry (function intersection) 'vector) test-class))
+
+(define-test test/all ()
+  "All the set tests."
+  (test/all/nil)
+  (test/all/sequence 'list)
+  (test/all/sequence 'vector)
+  (test/all/class    'list-set))
+
+
+;;;; THE END ;;;;
+
