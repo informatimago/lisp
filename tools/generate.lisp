ba4896c862f65f4e9348bc2f6e455bc8310df617
Pascal J. Bourguignon <pjb@informatimago.com>
2016-01-24 18:15:39 +0100

    Added tools/generate.lisp

---
+++ tools/generate.lisp
@@ -0,0 +1,23 @@
+;; (quick-load-all)
+
+(defun save-program (name &key init-file main)
+  #+ccl (ccl::save-application name
+                               :toplevel-function (when main
+                                                    (lambda ()
+                                                      (handler-case
+                                                          (funcall main (rest ccl:*command-line-argument-list*))
+                                                        (error (err)
+                                                          (finish-output *standard-output*)
+                                                          (finish-output *trace-output*)
+                                                          (format *error-output* "~%~A~%" err)
+                                                          (finish-output *error-output*)
+                                                          (ccl:quit 1)))
+                                                      (finish-output *standard-output*)
+                                                      (finish-output *trace-output*)
+                                                      (finish-output *error-output*)
+                                                      (ccl:quit 0)))
+                               :init-file init-file
+                               :mode #o755
+                               :prepend-kernel t
+                               :error-handler t)
+  #-(or ccl) (error "Not implemented for ~A" (lisp-implementation-type)))
