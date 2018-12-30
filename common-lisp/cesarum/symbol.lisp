97d74cda22345b91671656d3b9b57b1270d4b5cc
Pascal J. Bourguignon <pjb@informatimago.com>
2018-12-30 21:05:52 +0100

    First check-in.

---
+++ common-lisp/cesarum/symbol.lisp
@@ -0,0 +1,78 @@
+;;;; -*- mode:lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               symbol.lisp
+;;;;LANGUAGE:           Common-Lisp
+;;;;SYSTEM:             Common-Lisp
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    Export symbol functions.
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
+;;;;MODIFICATIONS
+;;;;    2018-12-30 <PJB> Created.
+;;;;BUGS
+;;;;LEGAL
+;;;;    AGPL3
+;;;;    
+;;;;    Copyright Pascal J. Bourguignon 2018 - 2018
+;;;;    
+;;;;    This program is free software: you can redistribute it and/or modify
+;;;;    it under the terms of the GNU Affero General Public License as published by
+;;;;    the Free Software Foundation, either version 3 of the License, or
+;;;;    (at your option) any later version.
+;;;;    
+;;;;    This program is distributed in the hope that it will be useful,
+;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;;    GNU Affero General Public License for more details.
+;;;;    
+;;;;    You should have received a copy of the GNU Affero General Public License
+;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;**************************************************************************
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf *readtable* (copy-readtable nil)))
+(in-package "COMMON-LISP-USER")
+(declaim (declaration also-use-packages))
+(declaim (also-use-packages "COM.INFORMATIMAGO.COMMON-LISP.CESARUM.SYMBOL"))
+(defpackage "COM.INFORMATIMAGO.COMMON-LISP.CESARUM.SYMBOL"
+  (:use "COMMON-LISP")
+  (:export
+   "SCAT")
+  (:documentation
+   "
+
+This package exports some symbol processing functions.
+
+
+License:
+
+    AGPL3
+
+    Copyright Pascal J. Bourguignon 2018 - 2018
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.
+    If not, see <http://www.gnu.org/licenses/>
+
+"))
+(in-package "COM.INFORMATIMAGO.COMMON-LISP.CESARUM.SYMBOL")
+
+
+(defun scat (&rest string-designators)
+  "Interns the concatenation of the STRING-DESIGNATORS."
+  (intern (apply (function concatenate) 'string
+                 (mapcar (function string) string-designators))))
+
+;;;; THE END ;;;;
