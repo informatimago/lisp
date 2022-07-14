a89d42805a749f571550f17072197119fc9a6f38
Pascal J. Bourguignon <pjb@informatimago.com>
2022-07-14 22:59:09 +0200

    Bumped copyright date.

--- common-lisp/lisp/stepper.lisp
+++ common-lisp/lisp/stepper.lisp
@@ -16,7 +16,7 @@
 ;;;;LEGAL
 ;;;;    AGPL3
 ;;;;
-;;;;    Copyright Pascal J. Bourguignon 2012 - 2016
+;;;;    Copyright Pascal J. Bourguignon 2012 - 2022
 ;;;;
 ;;;;    This program is free software: you can redistribute it and/or modify
 ;;;;    it under the terms of the GNU Affero General Public License as published by
